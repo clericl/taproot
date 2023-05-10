3aec3116fc85cef965c8d8e24c51d42c14e15275
Eric L <eliang58@gmail.com>
2023-05-09 20:54:59 -0400

    update map marker image

--- components/TreeMarker/index.tsx
+++ components/TreeMarker/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import {Marker} from 'react-native-maps';
 import {TreeDatumType} from '../../utils/types';
 
+const iconRequire = require('../../assets/green-marker.png');
+
 type TreeMarkerProps = {
   treeDatum: TreeDatumType;
 };
 
 function TreeMarker({treeDatum}: TreeMarkerProps) {
-  return <Marker title={treeDatum.species} coordinate={treeDatum.location} />;
+  return (
+    <Marker
+      title={treeDatum.species}
+      coordinate={treeDatum.location}
+      icon={iconRequire}
+    />
+  );
 }
 
 export default TreeMarker;
