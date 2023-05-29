91fec7ec1f698b157d8363c91b696692f3b2ebdc
Eric L <eliang58@gmail.com>
2023-05-28 21:07:25 -0400

    adjust bottom sheet styling

--- components/TreeDetailDrawer/index.tsx
+++ components/TreeDetailDrawer/index.tsx
@@ -1,14 +1,7 @@
 import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import BottomSheet, {BottomSheetBackgroundProps} from '@gorhom/bottom-sheet';
 import {BASE_IMAGE_URL} from '@env';
-import {
-  Dimensions,
-  Image,
-  Pressable,
-  Text,
-  View,
-  StyleSheet,
-} from 'react-native';
+import {Image, Pressable, Text, View, StyleSheet} from 'react-native';
 import {SpeciesDetailsType} from '../../types';
 import {useAppSelector} from '../../redux/utils/hooks';
 
@@ -29,7 +22,7 @@ function TreeDetailDrawerBackground({
     treeDetailData && (
       <View style={[defaultProps.style, styles.drawerBackground]}>
         <Image
-          style={styles.treePhoto}
+          style={[styles.treePhoto]}
           source={{
             uri: BASE_IMAGE_URL + treeDetailData.spc_latin + '.jpg',
           }}
@@ -44,10 +37,16 @@ function TreeDetailDrawer() {
   const treeDetailData = useAppSelector(state => state.treeDetail.data);
   const ref = useRef<BottomSheet>(null);
 
-  const snapPoints = useMemo(() => [140, '75%'], []);
-  const containerStyles = useMemo(
-    () => styler(allowTouch).container,
-    [allowTouch],
+  const snapPoints = useMemo(() => {
+    if (treeDetailData?.status === 'Alive') {
+      return [140, '75%'];
+    }
+    return [140, 141];
+  }, [treeDetailData]);
+
+  const overrideStyles = useMemo(
+    () => styler(allowTouch, treeDetailData?.status === 'Dead'),
+    [allowTouch, treeDetailData],
   );
 
   const BackgroundComponent = useCallback(
@@ -71,10 +70,31 @@ function TreeDetailDrawer() {
     ref.current?.snapToIndex(0);
   }, []);
 
+  const titleName = useMemo(() => {
+    if (treeDetailData) {
+      const latinName = treeDetailData.spc_latin;
+
+      if (!latinName) {
+        if (treeDetailData.status !== 'Alive') {
+          return treeDetailData.status;
+        } else {
+          return 'Unknown';
+        }
+      }
+
+      return latinName;
+    }
+
+    return '';
+  }, [treeDetailData]);
+
   const commonNames = useMemo(() => {
     if (treeDetailData) {
       const speciesDetail = speciesDetails[treeDetailData.spc_latin];
-      return speciesDetail.commonNames;
+
+      if (speciesDetail) {
+        return speciesDetail.commonNames;
+      }
     }
 
     return '';
@@ -82,14 +102,18 @@ function TreeDetailDrawer() {
 
   useEffect(() => {
     if (treeDetailData) {
-      ref.current?.snapToIndex(1);
+      if (treeDetailData.status === 'Alive') {
+        ref.current?.snapToIndex(1);
+      } else {
+        ref.current?.snapToIndex(0);
+      }
     } else {
       ref.current?.close();
     }
   }, [treeDetailData]);
 
   return (
-    <Pressable onPress={handleOuterPress} style={containerStyles}>
+    <Pressable onPress={handleOuterPress} style={overrideStyles.container}>
       <BottomSheet
         backgroundComponent={BackgroundComponent}
         handleIndicatorStyle={styles.handleIndicator}
@@ -98,11 +122,11 @@ function TreeDetailDrawer() {
         ref={ref}
         onChange={handleSheetChanges}
         snapPoints={snapPoints}>
-        <Pressable onPress={handleInnerPress} style={styles.contentContainer}>
+        <Pressable
+          onPress={handleInnerPress}
+          style={[styles.contentContainer, overrideStyles.contentContainer]}>
           <View style={styles.titleContainer}>
-            <Text style={[styles.text, styles.latin]}>
-              {treeDetailData ? treeDetailData.spc_latin : ''}
-            </Text>
+            <Text style={[styles.text, styles.latin]}>{titleName}</Text>
             <Text style={[styles.text, styles.common]}>{commonNames}</Text>
           </View>
         </Pressable>
@@ -111,13 +135,16 @@ function TreeDetailDrawer() {
   );
 }
 
-const styler = (allowTouch: boolean) =>
+const styler = (allowTouch: boolean, small: boolean) =>
   StyleSheet.create({
     container: {
       flex: 1,
       width: '100%',
       backgroundColor: 'transparent',
       pointerEvents: allowTouch ? 'auto' : 'box-none',
+    },
+    contentContainer: {
+      height: small ? 140 : '75%',
     },
   });
 
@@ -139,8 +166,7 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
   },
   treePhoto: {
-    height: Dimensions.get('window').height,
-    width: Dimensions.get('window').height * 0.67,
+    height: '43%',
     opacity: 0.6,
   },
   content: {
